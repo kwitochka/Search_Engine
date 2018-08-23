b168114be15f4eea460e9ef1607c68c49e8120df
kwitochka <kwitochka@hotmail.co.jp>
2018-08-23 22:39:10 +0300

    Adds alert message and calls IIFE

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-const model = {
-  currentEngine: null,
-  currentQuery: null,
-  google: {
-    url: "https://www.google.com.ua/search?q="
-  },
-  bing: {
-    url: "https://bing.com/search?q="
-  },
-  ask: {
-    url: "http://ask.com/web/?q="
-  }
-};
+(function() {
+  const model = {
+    currentEngine: null,
+    currentQuery: null,
+    google: {
+      url: "https://www.google.com.ua/search?q="
+    },
+    bing: {
+      url: "https://bing.com/search?q="
+    },
+    ask: {
+      url: "http://ask.com/web/?q="
+    }
+  };
 
-const octopus = {
-  setEngine: function(searchEngine) {
-    model.currentEngine = model[searchEngine];
-    searchView.render();
-  },
+  const controller = {
+    setEngine: function(searchEngine) {
+      model.currentEngine = model[searchEngine];
+      searchView.render();
+    },
 
-  getEngine: function() {
-    return model.currentEngine;
-  },
+    getEngine: function() {
+      return model.currentEngine;
+    },
 
-  setQuery: function(query) {
-    model.currentQuery = query.split(" ");
-  },
+    setQuery: function(query) {
+      model.currentQuery = query.split(" ");
+    },
 
-  getQuery: function() {
-    return model.currentQuery;
-  }
-};
+    getQuery: function() {
+      return model.currentQuery;
+    }
+  };
 
-const searchView = {
-  init: function() {
-    const searchBtn = document.getElementById("submit");
-    const selectElem = document.getElementById("search--engine");
-    this.input = document.getElementById("query");
+  const searchView = {
+    init: function() {
+      const searchBtn = document.getElementById("submit");
+      const selectElem = document.getElementById("search--engine");
+      this.input = document.getElementById("query");
 
-    //initialize Meterialise select
-    const options = document.querySelectorAll("option");
-    const instances = M.FormSelect.init(selectElem, options);
+      //initialize Meterialise select
+      const options = document.querySelectorAll("option");
+      const instances = M.FormSelect.init(selectElem, options);
 
-    let query, searchEngine;
+      let query, searchEngine;
 
-    searchBtn.addEventListener("click", function() {
-      if (selectElem.selectedIndex === 0) {
-        return false;
-      }
+      searchBtn.addEventListener("click", function() {
+        if (selectElem.selectedIndex === 0) {
+          alert("Укажите поисковую систему");
+          return false;
+        }
 
-      searchEngine = selectElem.options[selectElem.selectedIndex].value;
-      query = document.getElementById("query").value;
-      octopus.setQuery(query);
-      octopus.setEngine(searchEngine);
-    });
-  },
+        searchEngine = selectElem.options[selectElem.selectedIndex].value;
+        query = document.getElementById("query").value;
+        controller.setQuery(query);
+        controller.setEngine(searchEngine);
+      });
+    },
 
-  render: function() {
-    const url = octopus.getEngine().url;
-    const queries = octopus.getQuery().join("+");
-    // window.open(url+queries, '_blank');
+    render: function() {
+      const url = controller.getEngine().url;
+      const queries = controller.getQuery().join("+");
 
-    //to open in a new tab
-    const win = window.open(url + queries, "_blank");
-    win.focus();
+      //to open in a new tab
+      const win = window.open(url + queries, "_blank");
+      win.focus();
 
-    this.input.value = "";
-  }
-};
+      this.input.value = "";
+    }
+  };
 
-searchView.init();
+  searchView.init();
+})();
